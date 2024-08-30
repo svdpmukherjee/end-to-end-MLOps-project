7151d7109568f9c8615de3a9e2335ce04b6370e9
svdpmukherjee <100602182+svdpmukherjee@users.noreply.github.com>
2024-08-30 14:59:10 +0200

    project setup

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Chest-Cancer-Classification-Project"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "svdpmukherjee"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "svdp.mukherjee@gmail.com"
 
 
 setuptools.setup(
